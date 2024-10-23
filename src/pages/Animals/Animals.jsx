ab99d52fe76e1c18ae0811d82f403f0cbb98615a
Mauro0Mau <mauriciodasilvadesouza7270@gmail.com>
2024-10-23 20:19:49 -0300

    malfunctioning pages
    Trying to adjust them

--- src/pages/Animals/Animals.jsx
+++ src/pages/Animals/Animals.jsx
@@ -1,8 +1,6 @@
 import { Header } from '../../components/layout/Header/Header'
 import { Footer } from '../../components/layout/Footer/Footer'
-import { Card } from "../../components/layout/CardAnimals/CardAnimals";
-
-import styles from './Animals.module.css';
+import { Card } from '../../components/layout/CardAnimals/CardAnimals';
 
 export function Animals() {
     return (
